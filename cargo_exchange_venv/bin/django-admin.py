8ca93fd9da2325b8c84289f9639b38a42351f06b
Hannan <abhannan@hotmail.com>
2017-03-31 18:52:41 -0400

    add files necessary to deploy to heroku

---
+++ cargo_exchange_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Hannan/Documents/Projects/ACE/air_cargo_exchange/cargo_exchange/cargo_exchange_venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
